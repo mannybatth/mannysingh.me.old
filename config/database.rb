6ccfeedad6274182118a107ad0e80aef7cbc3fad
Manpreet <likebeats@gmail.com>
2015-01-26 18:03:09 -0500

    database

--- config/database.rb
+++ config/database.rb
@@ -1,8 +1,9 @@
 if ENV['MONGOLAB_URI']
 
     uri = URI.parse(ENV['MONGOLAB_URI'])
-    MongoMapper.connection = Mongo::Connection.from_uri(ENV['MONGOLAB_URI'])
+    MongoMapper.connection = Mongo::Connection.from_uri(uri)
     MongoMapper.database = uri.path.gsub(/^\//, '')
+    MongoMapper.authenticate(uri.user, uri.password)
 
 else
 
