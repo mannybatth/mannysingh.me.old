8cf54e523c08ed88695c2f88b619427f433ae72c
Manpreet <likebeats@gmail.com>
2015-01-26 23:52:54 -0500

    model

--- app/models/project.rb
+++ app/models/project.rb
@@ -22,3 +22,30 @@ class Project
     has n, :libraries, :through => Resource
     has n, :platforms, :through => Resource
 end
+
+
+# class Project
+#     include DataMapper::Resource
+
+#     property :id, Serial
+#     property :title, String, :required => true
+#     property :body, String
+#     property :screenshots, CommaSeparatedList
+#     # property :language_ids, CommaSeparatedList
+#     # property :library_ids, CommaSeparatedList
+#     # property :platform_ids, CommaSeparatedList
+    
+#     property :created_at, DateTime
+#     property :created_on, Date
+#     property :updated_at, DateTime
+#     property :updated_on, Date
+
+#     # many :languages, :in => :language_ids
+#     # many :libraries, :in => :library_ids
+#     # many :platforms, :in => :platform_ids
+    
+#     has n, :languages, :through => Resource
+#     has n, :libraries, :through => Resource
+#     has n, :platforms, :through => Resource
+# end
+
