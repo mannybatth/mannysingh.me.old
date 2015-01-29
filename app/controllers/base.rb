ff8f5226d03b510c16dc15d2767d90141f750282
Manpreet <likebeats@gmail.com>
2015-01-28 21:15:46 -0500

    view changes

--- app/controllers/base.rb
+++ app/controllers/base.rb
@@ -1,7 +1,7 @@
 MannySinghMe::App.controllers :base do
 
   get :index, :map => "/" do
-    @projects = Project.all(:order => 'created_at')
+    @projects = Project.all(:order => :created_at.desc)
     render "base/index"
   end
   
